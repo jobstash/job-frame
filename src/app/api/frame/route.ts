8cc0b2972cdab01bc1ff5110f9a9b15df7b2810c
John Ballesteros <johnshift.dev@gmail.com>
2024-01-30 09:49:02 +0800

    refactor: remove params check on route post handler
    post handler dont have params lel

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getFrameValidatedMessage } from '@coinbase/onchainkit';
-import { JOB_TAB_PARAMS_SET, JobTabParamsText } from '~/core/constants';
 
 export const dynamic = 'force-dynamic';
 
@@ -8,24 +7,17 @@ const FE_URL = process.env.FE_URL;
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   try {
-    const tab = req.nextUrl.searchParams.get('tab') as JobTabParamsText | null;
-    if (!tab) throw new Error('"tab" param is required');
-
-    const isInTabSet = JOB_TAB_PARAMS_SET.has(tab);
-    if (!isInTabSet) throw new Error(`Invalid tab "${tab}"`);
-
-    const id = req.nextUrl.searchParams.get('id');
-    if (!id) throw new Error('"id" param is required');
-
     const body = await req.json();
     const validatedMessage = await getFrameValidatedMessage(body);
 
     if (validatedMessage) {
-      console.log({ id, tab, validatedMessage });
+      console.log({ validatedMessage });
 
-      // Redirect to page w/ frame metadata
-      const url = `${FE_URL}/jobs/${id}/${tab}`;
-      return NextResponse.redirect(url);
+      // // Redirect to page w/ frame metadata
+      // const url = `${FE_URL}/jobs/${id}/${tab}`;
+      // return NextResponse.redirect(url);
+
+      return NextResponse.json({ message: 'OK' });
     }
 
     throw new Error('Invalid frame message');
